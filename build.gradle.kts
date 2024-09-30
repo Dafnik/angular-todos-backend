6f84d62f4aa2383e39255e7a98c83d43160e767c
Dafnik <git@dafnik.me>
2024-09-30 14:07:02 +0200

    ci: fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,21 +45,12 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.2")
-
-
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 kotlin {
 	compilerOptions {
 		freeCompilerArgs.addAll("-Xjsr305=strict")
 	}
-}
-
-tasks.withType<Test> {
-	useJUnitPlatform()
 }
 
 // needed to skip generation of plain jar
